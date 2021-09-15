bee19d942a1c9bffa261af5c53df211791cba405
Jhoe73 <jhoenascimento73@gmail.com>
2021-09-15 01:42:20 -0300

    project fonts

--- src/Constants/theme.ts
+++ src/Constants/theme.ts
@@ -14,6 +14,20 @@ const theme = createTheme({
       main: red.A400,
     },
   },
+  typography: {
+    fontFamily: [
+      'Lexend Deca',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
 });
 
 export default theme;
